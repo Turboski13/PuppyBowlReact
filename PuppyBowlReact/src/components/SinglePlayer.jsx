814239d2648fdb97bc27e35fb86dba1fbbbe1867
Jessica Lafferty <jessica.a.lafferty@gmail.com>
2024-09-03 17:00:23 -0400

    day 3 try 3

--- PuppyBowlReact/src/components/SinglePlayer.jsx
+++ PuppyBowlReact/src/components/SinglePlayer.jsx
@@ -1,14 +1,38 @@
+const cohortName = "2405-ftb-et-web-pt";
+const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}`;
 
-export default function SinglePlayer() {
-    return (
-      <div className="singleplayer">
-        <h1>The Single Players Component</h1>
-      </div>
+
+const SinglePlayer = () => {
+  const [player, setPlayer] = useState([]) 
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    const fetchPlayer = async (playerId) => {
+      const playerData = await PuppyBowlApi();
+      setPlayer(playerData);
+  };
+  fetchPlayer();
+  }, []);
+
+  
+  return (
+    <div>
+    {
+        player.map((player)=>{
+            return (
+                <div>
+                    <h4>{player.name}</h4>
+                    <h4>{player.breed}</h4>
+                    <h4>{player.status}</h4>
+                    <h4>{player.teamID}</h4>
+                    <img src={player.imageUrl} alt="{player.name}" />
+                </div>
+            )
+        })
+    }
+</div>
     );
   }
-
-
-
 
 
   
